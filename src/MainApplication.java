e671490ee4b8910fc4e7a73cc8cbfdf50ed427bc
beeccy <beeccy@Rebeccas-MacBook-Pro.local>
2015-12-02 20:32:08 +0100

    Scanner input inserted

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,36 +1,63 @@
+import java.util.Scanner;
+
 /**
  * Created by beeccy on 02/12/15.
  */
 public class MainApplication {
     public static void main(String[] args) {
-        double hourlyPay = 160;
-        double hoursWorked = 8.5;
-        double lunchBreak = 0.5;
-        double taxPercentage = 0.75;
-        double holidayPercentage = 0.78;
+        double percentage = 1;
 
-        double calculatedPay = calculatePay(hoursWorked, hourlyPay, lunchBreak);
-        double finishedSalary = SalaryAfterTax(calculatedPay, taxPercentage, holidayPercentage);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hourly pay: ");
+        double hourlyPay = scanner.nextDouble();
+        System.out.println("Hours worked: ");
+        double hoursWorked = scanner.nextDouble();
+        System.out.println("Lunch break: ");
+        double lunchBreak = scanner.nextDouble();
+        System.out.println("How many days? ");
+        double daysTotal = scanner.nextDouble();
+        System.out.println("Tax percentage (written 0.xx): ");
+        double taxPercentage = percentage - scanner.nextDouble();
+
+        double calculatedPay = calculatePay(hoursWorked, hourlyPay, lunchBreak, daysTotal);
+        double finishedSalary = SalaryAfterTax(calculatedPay, taxPercentage);
+
+        String americanDollar = "$";
+        String japaneseYen = "¥";
+        String britishPounds = "£";
+        String euro = "€";
+        String norwegianCrown = " kr";
 
         Print(calculatedPay);
-        Print(finishedSalary);
+        System.out.println(finishedSalary + norwegianCrown);
+
+        System.out.println(nokToUsd(finishedSalary) + americanDollar);
+
+    }
+
+    private static double nokToUsd(double salaryUSDollars) {
+        double currency = ;
+        salaryUSDollars = currency * salaryUSDollars;
+
+        return salaryUSDollars;
     }
 
     /* This calculates time correctly */
-    public static double calculatePay(double hours, double pay, double lunch) {
+    public static double calculatePay(double hours, double pay, double lunch, double days) {
         double toAvoidDesimals = 100;
 
         double workHours = hours * toAvoidDesimals;
 
-        double timeCalculated = ((workHours - lunch) * pay) / toAvoidDesimals;
+        double timeCalculated = ((workHours - lunch) * pay) * days / toAvoidDesimals;
 
         return timeCalculated;
     }
 
-    public static double SalaryAfterTax(double salary, double tax, double holiday) {
+    public static double SalaryAfterTax(double salary, double tax) {
         double toAvoidDesimals = 100;
 
-        double calculateSalary = (salary * tax) * holiday;
+        double calculateSalary = (salary * tax);
 
         return calculateSalary;
     }
